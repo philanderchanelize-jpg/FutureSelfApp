968d0880a3a0bb3a7a041a2d79aa5d16cccd66f2
philanderchanelize-jpg <philanderchanelize@gmail.com>
2026-08-05 19:48:19 +0200

    Create initial PySide6 GUI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
 import sys
 
 app = QApplication(sys.argv)
@@ -8,8 +8,17 @@ window.setWindowTitle("Future Self")
 window.resize(800, 600)
 
 label = QLabel("Welcome to Future Self")
+
+journal_button = QPushButton("New Journal")
+goals_button = QPushButton("My Goals")
+exit_button = QPushButton("Exit")
+
 layout = QVBoxLayout()
 layout.addWidget(label)
+layout.addWidget(journal_button)
+layout.addWidget(goals_button)
+layout.addWidget(exit_button)
+
 window.setLayout(layout)
 
 window.show()
